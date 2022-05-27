29e01eb0608e7649ce4f894de1a15c29e78a7660
danocolombo@google.com <GR0mans1212!>
2022-05-27 17:57:00 -0400

    running on desktop

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,35 +1,12 @@
 import React from 'react';
-import { useQuery } from 'react-query';
-import RalliesOutput from '../components/rallies/RalliesOutput';
-export default function Characters() {
-    const fetchCharacters = async () => {
-        const response = await fetch(
-            'https://j7qty6ijwg.execute-api.us-east-1.amazonaws.com/QA/events',
-            {
-                method: 'POST',
-                body: JSON.stringify({
-                    operation: 'getAllActiveApprovedEvents',
-                }),
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8',
-                },
-            }
-        );
-        return response.json();
-    };
-    // characters is just a unique key for the query.
-    const { data, status } = useQuery('characters', fetchCharacters);
-
-    if (status === 'loading') {
-        return <div>LOADING</div>;
-    }
-    if (status === 'error') {
-        return <div>ERROR received....</div>;
-    }
-
+import { View, Text } from 'react-native';
+function ServeScreen() {
     return (
         <>
-            <RalliesOutput rallies={data.body} />
+            <View>
+                <Text>Serve Screen</Text>
+            </View>
         </>
     );
 }
+export default ServeScreen;
