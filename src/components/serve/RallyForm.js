771869e2b4ba4156311d652338b2db8f95e72a68
Dano Colombo <danocolombo@gmail.com>
2022-05-31 05:31:26 -0400

    rallyForm dismiss keyboard on click

--- src/components/serve/RallyForm.js
+++ src/components/serve/RallyForm.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { StyleSheet, TextInput, View, Text } from 'react-native';
+import {
+    StyleSheet,
+    TextInput,
+    View,
+    Text,
+    TouchableWithoutFeedback,
+    Keyboard,
+} from 'react-native';
 import { Button } from '@react-native-material/core';
 import { useDispatch } from 'react-redux';
 import { Colors } from '../../constants/colors.js';
@@ -11,93 +18,102 @@ export default function ReviewForm({}) {
     const dispatch = useDispatch();
     return (
         <View>
-            <Text>Location Section</Text>
-            <Formik
-                initialValues={{
-                    locationName: '',
-                    street: '',
-                    city: '',
-                    stateProv: 'GA',
-                    postalCode: '',
-                }}
-                onSubmit={(values, actions) => {
-                    actions.resetForm();
-                    let dbUpdateResults = async () => {
-                        //update dynamo
-                        putRally(values);
-                    };
-                    dbUpdateResults()
-                        .then((results) => {
-                            dispatch(addNewRally, actions);
-                        })
-                        .then(() => {
-                            console.log('these values were saved: \n', values);
-                        })
-                        .catch((err) => {
-                            console.log('We got error\n:', err);
-                        });
-                }}
-            >
-                {(formikProps) => (
-                    <>
-                        <View>
-                            <TextInput
-                                style={styles.input}
-                                placeholder='Location Name'
-                                onChangeText={formikProps.handleChange(
-                                    'locationName'
-                                )}
-                                value={formikProps.values.locationName}
-                            />
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View>
+                    <Text>Location Section</Text>
+                    <Formik
+                        initialValues={{
+                            locationName: '',
+                            street: '',
+                            city: '',
+                            stateProv: 'GA',
+                            postalCode: '',
+                        }}
+                        onSubmit={(values, actions) => {
+                            actions.resetForm();
+                            let dbUpdateResults = async () => {
+                                //update dynamo
+                                putRally(values);
+                            };
+                            dbUpdateResults()
+                                .then((results) => {
+                                    dispatch(addNewRally, actions);
+                                })
+                                .then(() => {
+                                    console.log(
+                                        'these values were saved: \n',
+                                        values
+                                    );
+                                })
+                                .catch((err) => {
+                                    console.log('We got error\n:', err);
+                                });
+                        }}
+                    >
+                        {(formikProps) => (
+                            <>
+                                <View>
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder='Location Name'
+                                        onChangeText={formikProps.handleChange(
+                                            'locationName'
+                                        )}
+                                        value={formikProps.values.locationName}
+                                    />
 
-                            <TextInput
-                                style={styles.input}
-                                placeholder='Street'
-                                onChangeText={formikProps.handleChange(
-                                    'street'
-                                )}
-                                value={formikProps.values.street}
-                            />
-                            <TextInput
-                                style={styles.input}
-                                placeholder='City'
-                                onChangeText={formikProps.handleChange('city')}
-                                value={formikProps.values.city}
-                            />
-                            <TextInput
-                                style={styles.input}
-                                placeholder='STATE'
-                                onChangeText={formikProps.handleChange(
-                                    'stateProv'
-                                )}
-                                value={formikProps.values.stateProv}
-                            />
-                            <TextInput
-                                style={styles.input}
-                                placeholder='Zipcode'
-                                onChangeText={formikProps.handleChange(
-                                    'postalCode'
-                                )}
-                                keyboardType='numeric'
-                                value={formikProps.values.postalCode}
-                            />
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder='Street'
+                                        onChangeText={formikProps.handleChange(
+                                            'street'
+                                        )}
+                                        value={formikProps.values.street}
+                                    />
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder='City'
+                                        onChangeText={formikProps.handleChange(
+                                            'city'
+                                        )}
+                                        value={formikProps.values.city}
+                                    />
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder='STATE'
+                                        onChangeText={formikProps.handleChange(
+                                            'stateProv'
+                                        )}
+                                        value={formikProps.values.stateProv}
+                                    />
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder='Zipcode'
+                                        onChangeText={formikProps.handleChange(
+                                            'postalCode'
+                                        )}
+                                        keyboardType='numeric'
+                                        value={formikProps.values.postalCode}
+                                    />
 
-                            <View style={styles.buttonContainer}>
-                                <Button
-                                    variant='contained'
-                                    title='SUBMIT'
-                                    color={Colors.mediumBackground}
-                                    tintColor='white'
-                                    style={styles.submitButton}
-                                    // style={styles.submitButton}
-                                    onPress={formikProps.handleSubmit}
-                                />
-                            </View>
-                        </View>
-                    </>
-                )}
-                {/* this ends the formik execution */}
-            </Formik>
+                                    <View style={styles.buttonContainer}>
+                                        <Button
+                                            variant='contained'
+                                            title='SUBMIT'
+                                            color={Colors.mediumBackground}
+                                            tintColor='white'
+                                            style={styles.submitButton}
+                                            // style={styles.submitButton}
+                                            onPress={formikProps.handleSubmit}
+                                        />
+                                    </View>
+                                </View>
+                            </>
+                        )}
+                        {/* this ends the formik execution */}
+                    </Formik>
+                </View>
+            </TouchableWithoutFeedback>
         </View>
     );
 }
