ae806a3008bc0ca75a420560620bd3085ae986e4
danocolombo@google.com <GR0mans1212!>
2022-05-30 21:46:52 -0400

    location form displaying

--- src/components/serve/RallyForm.js
+++ src/components/serve/RallyForm.js
@@ -1,122 +1,104 @@
 import React from 'react';
-import { StyleSheet, TextInput, Text, View } from 'react-native';
-import { Surface, Button } from '@react-native-material/core';
+import { StyleSheet, TextInput, View, Text } from 'react-native';
+import { Button } from '@react-native-material/core';
+import { Colors } from '../../constants/colors.js';
+import { globalStyles } from './FormikStyles.js';
 import { Formik } from 'formik';
-const RallyForm = () => {
+
+export default function ReviewForm({}) {
     return (
-        <View style={styles.container}>
-            <View style={styles.formContainer}>
-                <Surface
-                    elevation={6}
-                    category='medium'
-                    style={styles.registrationCard}
-                >
-                    <Formik
-                        initialValues={{ title: '', body: '', rating: '' }}
-                        onSubmit={(values, actions) => {
-                            actions.resetForm();
-                            console.log('values\n', values);
-                        }}
-                    >
-                        {(formikProps) => (
-                            <View>
-                                <TextInput
-                                    style={styles.input}
-                                    placeholder='Review Title'
-                                    onChangeText={formikProps.handleChange(
-                                        'title'
-                                    )}
-                                    value={formikProps.values.title}
+        <View>
+            <Text>Location Section</Text>
+            <Formik
+                initialValues={{
+                    locationName: '',
+                    street: '',
+                    city: '',
+                    stateProv: 'GA',
+                    postalCode: '',
+                }}
+                onSubmit={(values, actions) => {
+                    actions.resetForm();
+                    console.log('values:\n', values);
+                }}
+            >
+                {(formikProps) => (
+                    <>
+                        <View>
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Location Name'
+                                onChangeText={formikProps.handleChange(
+                                    'locationName'
+                                )}
+                                value={formikProps.values.locationName}
+                            />
+
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Street'
+                                onChangeText={formikProps.handleChange(
+                                    'street'
+                                )}
+                                value={formikProps.values.street}
+                            />
+                            <TextInput
+                                style={styles.input}
+                                placeholder='City'
+                                onChangeText={formikProps.handleChange('city')}
+                                value={formikProps.values.city}
+                            />
+                            <TextInput
+                                style={styles.input}
+                                placeholder='STATE'
+                                onChangeText={formikProps.handleChange(
+                                    'stateProv'
+                                )}
+                                value={formikProps.values.stateProv}
+                            />
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Zipcode'
+                                onChangeText={formikProps.handleChange(
+                                    'postalCode'
+                                )}
+                                keyboardType='numeric'
+                                value={formikProps.values.postalCode}
+                            />
+
+                            <View style={styles.buttonContainer}>
+                                <Button
+                                    variant='contained'
+                                    title='SUBMIT'
+                                    color={Colors.mediumBackground}
+                                    tintColor='white'
+                                    style={styles.submitButton}
+                                    // style={styles.submitButton}
+                                    onPress={formikProps.handleSubmit}
                                 />
-                                <TextInput
-                                    style={styles.input}
-                                    placeholder='Review Body'
-                                    onChangeText={formikProps.handleChange(
-                                        'body'
-                                    )}
-                                    value={formikProps.values.body}
-                                />
-                                <TextInput
-                                    style={styles.input}
-                                    placeholder='Rating (1-5)'
-                                    onChangeText={formikProps.handleChange(
-                                        'rating'
-                                    )}
-                                    keyboardType='numeric'
-                                    value={formikProps.values.rating}
-                                />
-                                <View style={styles.buttonContainer}>
-                                    <Button
-                                        variant='outlined'
-                                        title='submit'
-                                        color='white'
-                                        style={styles.submitButton}
-                                        onPress={() =>
-                                            formikProps.handleSubmit()
-                                        }
-                                    ></Button>
-                                </View>
                             </View>
-                        )}
-                    </Formik>
-                </Surface>
-            </View>
+                        </View>
+                    </>
+                )}
+                {/* this ends the formik execution */}
+            </Formik>
         </View>
     );
-};
-
-export default RallyForm;
-
+}
 const styles = StyleSheet.create({
-    titleText: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#333',
-    },
-    paragraph: {
-        marginVertical: 8,
-        lineHeight: 20,
-    },
-    container: {
-        // borderWidth: 1,
-        // borderColor: 'black',
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        padding: 10,
-    },
-    formContainer: {
-        // borderWidth: 1,
-        // borderColor: 'black',
-        width: '100%',
-        margin: 5,
-        alignItems: 'center',
-        paddingVertical: 5,
-    },
-    registrationCard: {
-        width: '90%',
-    },
     input: {
         borderWidth: 1,
-        borderColor: '#ddd',
+        borderColor: Colors.gray35,
         padding: 10,
+        marginHorizontal: 10,
         fontSize: 18,
         borderRadius: 6,
-        marginHorizontal: 10,
-        marginVertical: 5,
     },
     buttonContainer: {
         alignItems: 'center',
+        marginVertical: 10,
     },
     submitButton: {
-        backgroundColor: 'maroon',
-        width: '60%',
-    },
-    errorText: {
-        color: 'crimson',
-        fontWeight: 'bold',
-        marginBottom: 10,
-        marginTop: 6,
-        textAlign: 'center',
+        width: '70%',
     },
 });
