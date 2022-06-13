87ed4fb6641ccd5678ea4d38a073b86b3e7417fb
Dano Colombo <danocolombo@gmail.com>
2022-06-13 06:11:14 -0400

    saving to redux

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -2,23 +2,11 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { List, Badge } from 'react-native-paper';
 import CardDate from '../components/ui/RallyCardDate';
+import ProfileForm from '../components/Profile.Form';
 function ProfileScreen() {
     return (
         <>
-            <View>
-                <Text>Profile Screen</Text>
-            </View>
-            <View>
-                <List.Item
-                    title='First Item'
-                    description='Item description'
-                    left={(props) => <List.Icon {...props} icon='folder' />}
-                />
-                <Badge>3</Badge>
-            </View>
-            <View>
-                <CardDate date='20220601' />
-            </View>
+            <ProfileForm />
         </>
     );
 }
