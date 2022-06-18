e621d8084cfee8ff595027200778dde937978140
danocolombo@google.com <GR0mans1212!>
2022-06-18 17:14:52 -0400

    datetime pickers added to Logistics component

--- src/components/rallies/edit/Rally.Edit.Logistics.js
+++ src/components/rallies/edit/Rally.Edit.Logistics.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     StyleSheet,
     TextInput,
@@ -8,6 +8,8 @@ import {
     Keyboard,
     ScrollView,
 } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+// import { Picker } from 'react-native-windows';
 import { useNavigation } from '@react-navigation/native';
 import { Headline } from 'react-native-paper';
 import { Button } from '@react-native-material/core';
@@ -15,17 +17,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Colors } from '../../../constants/colors';
 // import { putRally } from '../../providers/rallies';
 import { updateTmp } from '../../../features/rallies/ralliesSlice';
-import { Formik } from 'formik';
-import * as yup from 'yup';
 import CustomNavButton from '../../ui/CustomNavButton';
 import { printObject } from '../../../utils/helpers';
-
-// create validation schema for yup to pass to formik
-const rallyLogisticsSchema = yup.object({
-    eventDate: yup.string().required().min(8),
-    startTime: yup.string(),
-    endTime: yup.string(),
-});
 
 export default function RallyLogisticsForm({ rallyId }) {
     const navigation = useNavigation();
@@ -35,6 +28,7 @@ export default function RallyLogisticsForm({ rallyId }) {
         state.rallies.allRallies.filter((r) => r.uid === rallyId)
     );
     const rally = rallyEntry[0];
+    const [date, setDate] = useState(new Date(Date.now()));
     const handleNext = (values) => {
         //printObject('handleNext::values', values);
         dispatch(updateTmp(values));
@@ -50,117 +44,93 @@ export default function RallyLogisticsForm({ rallyId }) {
                 <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                     <View>
                         <ScrollView>
-                            <Formik
-                                initialValues={{
-                                    eventDate: rally?.eventDate
-                                        ? rally.eventDate
-                                        : '',
-                                    startTime: rally?.startTime
-                                        ? rally.startTime
-                                        : '',
-                                    endTime: rally?.endTime
-                                        ? rally.endTime
-                                        : '',
-                                }}
-                                validationSchema={rallyLogisticsSchema}
-                                onSubmit={async (values, actions) => {
-                                    // printObject('onSubmit::values', values);
-                                    handleNext(values);
-                                }}
-                            >
-                                {(formikProps) => (
-                                    <>
-                                        <View style={styles.formHeader}>
-                                            <Headline>
-                                                Rally Logistics Information
-                                            </Headline>
-                                        </View>
-                                        <View style={styles.inputContainer}>
-                                            <View>
-                                                <TextInput
-                                                    style={styles.input}
-                                                    placeholder='Event Date'
-                                                    onChangeText={formikProps.handleChange(
-                                                        'eventDate'
-                                                    )}
-                                                    value={
-                                                        formikProps.values
-                                                            .eventDate
+                            <View style={styles.formHeader}>
+                                <Text style={styles.titleText}>Logistics</Text>
+                            </View>
+                            <View style={styles.inputContainer}>
+                                <View>
+                                    <View>
+                                        <View style={styles.inputLabels}>
+                                            <Text style={styles.inputLabelText}>
+                                                Event Date
+                                            </Text>
+                                            <View
+                                                style={styles.datePickerWrapper}
+                                            >
+                                                <DateTimePicker
+                                                    testID='dateTimePicker'
+                                                    value={date}
+                                                    mode='date'
+                                                    display={
+                                                        Platform.OS === 'ios'
+                                                            ? 'spinner'
+                                                            : 'default'
                                                     }
-                                                    onBlur={formikProps.handleBlur(
-                                                        'eventDate'
-                                                    )}
+                                                    is24Hour={true}
+                                                    //onChange={onChange}
+                                                    style={styles.datePicker}
                                                 />
-                                                <Text style={styles.errorText}>
-                                                    {formikProps.touched
-                                                        .eventDate &&
-                                                        formikProps.errors
-                                                            .eventDate}
-                                                </Text>
-                                                <TextInput
-                                                    style={styles.input}
-                                                    placeholder='Start Time'
-                                                    onChangeText={formikProps.handleChange(
-                                                        'startTime'
-                                                    )}
-                                                    value={
-                                                        formikProps.values
-                                                            .startTime
+                                            </View>
+                                            <Text style={styles.inputLabelText}>
+                                                Start Time
+                                            </Text>
+                                            <View
+                                                style={styles.datePickerWrapper}
+                                            >
+                                                <DateTimePicker
+                                                    testID='dateTimePicker'
+                                                    value={date}
+                                                    mode='time'
+                                                    display={
+                                                        Platform.OS === 'ios'
+                                                            ? 'spinner'
+                                                            : 'default'
                                                     }
-                                                    onBlur={formikProps.handleBlur(
-                                                        'startTime'
-                                                    )}
+                                                    is24Hour={true}
+                                                    //onChange={onChange}
+                                                    style={styles.datePicker}
                                                 />
-                                                <Text style={styles.errorText}>
-                                                    {formikProps.touched
-                                                        .startTime &&
-                                                        formikProps.errors
-                                                            .startTime}
-                                                </Text>
-                                                <TextInput
-                                                    style={styles.input}
-                                                    placeholder='End Time'
-                                                    onChangeText={formikProps.handleChange(
-                                                        'endTime'
-                                                    )}
-                                                    value={
-                                                        formikProps.values
-                                                            .endTime
+                                            </View>
+                                            <Text style={styles.inputLabelText}>
+                                                Finish Time
+                                            </Text>
+                                            <View
+                                                style={styles.datePickerWrapper}
+                                            >
+                                                <DateTimePicker
+                                                    testID='dateTimePicker'
+                                                    value={date}
+                                                    mode='time'
+                                                    display={
+                                                        Platform.OS === 'ios'
+                                                            ? 'spinner'
+                                                            : 'default'
                                                     }
-                                                    onBlur={formikProps.handleBlur(
-                                                        'endTime'
-                                                    )}
+                                                    is24Hour={true}
+                                                    //onChange={onChange}
+                                                    style={styles.datePicker}
                                                 />
-                                                <Text style={styles.errorText}>
-                                                    {formikProps.touched
-                                                        .endTime &&
-                                                        formikProps.errors
-                                                            .endTime}
-                                                </Text>
                                             </View>
                                         </View>
-                                        <View style={styles.buttonContainer}>
-                                            <CustomNavButton
-                                                title='Next'
-                                                graphic={{
-                                                    name: 'forward',
-                                                    color: 'white',
-                                                    size: 10,
-                                                }}
-                                                cbStyles={{
-                                                    backgroundColor: 'green',
-                                                    color: 'white',
-                                                    width: '50%',
-                                                }}
-                                                onPress={
-                                                    formikProps.handleSubmit
-                                                }
-                                            />
-                                        </View>
-                                    </>
-                                )}
-                                {/* this ends the formik execution */}
-                            </Formik>
+                                    </View>
+                                </View>
+                            </View>
+                            <View style={styles.buttonContainer}>
+                                <CustomNavButton
+                                    title='Next'
+                                    graphic={{
+                                        name: 'forward',
+                                        color: 'white',
+                                        size: 10,
+                                    }}
+                                    cbStyles={{
+                                        backgroundColor: 'green',
+                                        color: 'white',
+                                        width: '50%',
+                                    }}
+                                    onPress={handleNext}
+                                />
+                            </View>
                         </ScrollView>
                     </View>
                 </TouchableWithoutFeedback>
@@ -173,66 +143,17 @@ const styles = StyleSheet.create({
         marginVertical: 10,
         alignItems: 'center',
     },
-    inputContainer: {
-        marginLeft: '10%',
-    },
-    input: {
-        borderWidth: 1,
-        borderColor: 'grey',
-        padding: 10,
-        marginTop: 0,
-        fontSize: 18,
-        borderRadius: 6,
-        width: '90%',
-    },
-    inputStateProv: {
-        width: 75,
-    },
-    inputPostalCode: {
-        width: 125,
-    },
-    errorText: {
-        color: 'crimson',
+    titleText: {
+        fontSize: 28,
         fontWeight: 'bold',
-        marginBottom: 10,
-        marginTop: 5,
-        // textAlign: 'center',
     },
-    stateProvPostalCodeContainerRow: {
-        // borderWidth: 1,
-        // borderColor: 'black',
+    inputLabels: {
+        alignItems: 'center',
     },
-    stateProvPostalCodeContainer: {
-        flexDirection: 'row',
+    inputLabelText: {
+        fontSize: 24,
+        // fontWeight: 'bold',
     },
-    stateProvSectionContainer: {
-        // borderWidth: 1,
-        // borderColor: 'blue',
-        marginRight: 30,
-    },
-    stateProvInputContainer: {
-        // borderWidth: 1,
-        // borderColor: 'blue',
-    },
-    stateProvErrorContainer: {
-        //borderWidth: 1, borderColor: 'blue'
-    },
-    postalCodeSectionContainer: {
-        //borderWidth: 1, borderColor: 'black'
-    },
-    postalCodeInputContainer: {
-        //borderWidth: 1, borderColor: 'black'
-    },
-    postalCodeErrorContainer: {
-        //borderWidth: 1, borderColor: 'black'
-    },
-    stateContainer: {
-        backgroundColor: 'yellow',
-
-        borderWidth: 1,
-        borderColor: 'black',
-    },
-    postalCodeContainer: {},
     buttonContainer: {
         alignItems: 'center',
         marginTop: 10,
@@ -240,5 +161,23 @@ const styles = StyleSheet.create({
     },
     submitButton: {
         width: '70%',
+    },
+    datePickerWrapper: {
+        borderWidth: 4,
+        borderColor: Colors.gray35,
+        borderRadius: 10,
+        marginBottom: 5,
+    },
+    datePicker: {
+        width: 320,
+        height: 100,
+        // display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 0,
+        padding: 0,
+        // borderWidth: 3,
+        // borderStyle: 'double',
+        borderColor: Colors.gray700,
     },
 });
