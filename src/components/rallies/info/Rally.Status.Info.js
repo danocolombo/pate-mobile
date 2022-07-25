5406f460e714dd672411fdb92ffd9c5f3da23f4c
Dano Colombo <danocolombo@gmail.com>
2022-07-25 05:43:26 -0400

    collapsible added to Rally.Status.Info

--- src/components/rallies/info/Rally.Status.Info.js
+++ src/components/rallies/info/Rally.Status.Info.js
@@ -1,12 +1,17 @@
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { Surface, Headline, Subheading } from 'react-native-paper';
 import { Badge } from 'react-native-paper';
+//import for the animation of Collapse and Expand
+import * as Animatable from 'react-native-animatable';
+import { AntDesign } from '@expo/vector-icons';
+import Collapsible from 'react-native-collapsible';
 import CustomSmallButton from '../../ui/CustomSmallButton';
 import { Colors } from '../../../constants/colors';
 import { printObject } from '../../../utils/helpers';
 const RallyStatusInfo = ({ rally, onPress }) => {
     // printObject('RSI:9 --> rally', rally);
+    const [collapsed, setCollapsed] = useState(true); //collapsible
     let tColor = 'gold';
     let bColor = 'blue';
 
@@ -31,56 +36,105 @@ const RallyStatusInfo = ({ rally, onPress }) => {
         default:
             break;
     }
+    const toggleExpanded = () => {
+        //Toggling the state of single Collapsible
+        setCollapsed(!collapsed);
+    };
+    const renderContent = (section, _, isActive) => {
+        //Accordion Content view
+        return (
+            <Animatable.View
+                duration={400}
+                style={[
+                    styles.content,
+                    isActive ? styles.active : styles.inactive,
+                ]}
+                transition='backgroundColor'
+            >
+                <Animatable.Text
+                    animation={isActive ? 'bounceIn' : undefined}
+                    style={{ textAlign: 'center' }}
+                >
+                    NOW YOU SEE ME
+                </Animatable.Text>
+            </Animatable.View>
+        );
+    };
     return (
         <>
             <View style={styles.rootContainer}>
-                <Surface style={[styles.surface, { elevation: 5 }]}>
-                    <View style={styles.statusOutline}>
-                        <View style={styles.statusDataWrapper}>
-                            <View style={styles.statusRow}>
-                                <View style={styles.statusTitle}>
-                                    <Text style={styles.statusTitleText}>
-                                        Event Details
-                                    </Text>
-                                </View>
-                            </View>
-                            <View style={styles.statusRow}>
-                                <CustomSmallButton
-                                    title={rally?.status}
-                                    cbStyles={{
-                                        backgroundColor: bColor,
-                                        color: tColor,
-                                    }}
-                                    onPress={onPress}
+                {/*Code for Single Collapsible Start*/}
+                <TouchableOpacity onPress={toggleExpanded}>
+                    <View style={styles.collapsibleHeaderRow}>
+                        <Text style={styles.collapsibleHeaderText}>
+                            Event Details
+                        </Text>
+                        <View style={styles.collapsibleHeaderIcon}>
+                            {collapsed ? (
+                                <AntDesign
+                                    name='caretright'
+                                    size={18}
+                                    color={Colors.gray60}
                                 />
-
-                                <Text style={styles.textWrapper}>
-                                    Approved:{' '}
-                                    {rally?.approved === true ? 'Yes' : 'No'}
-                                </Text>
-                            </View>
-                            <View style={styles.statusRow}>
-                                <Text style={styles.textWrapper}>
-                                    Region:
-                                    {rally.region}
-                                </Text>
-                                <Text style={styles.textWrapper}>
-                                    EventRegion: {rally.eventRegion}
-                                </Text>
-                            </View>
-                            <View style={styles.statusRow}>
-                                <Text style={styles.textWrapper}>
-                                    UID: {rally.uid}
-                                </Text>
-                            </View>
-                            <View style={styles.statusRow}>
-                                <Text style={styles.textWrapper}>
-                                    Coordinator: {rally?.coordinator?.name}
-                                </Text>
-                            </View>
+                            ) : (
+                                <AntDesign
+                                    name='caretdown'
+                                    size={18}
+                                    color={Colors.gray60}
+                                />
+                            )}
                         </View>
                     </View>
-                </Surface>
+                </TouchableOpacity>
+                {/*Content of Single Collapsible*/}
+                <Collapsible collapsed={collapsed} align='center'>
+                    <View style={styles.detailContainer}>
+                        <Surface style={[styles.surface, { elevation: 5 }]}>
+                            <View style={styles.statusOutline}>
+                                <View style={styles.statusDataWrapper}>
+                                    <View style={styles.statusRow}>
+                                        <CustomSmallButton
+                                            title={rally?.status}
+                                            cbStyles={{
+                                                backgroundColor: bColor,
+                                                color: tColor,
+                                            }}
+                                            onPress={onPress}
+                                        />
+
+                                        <Text style={styles.textWrapper}>
+                                            Approved:{' '}
+                                            {rally?.approved === true
+                                                ? 'Yes'
+                                                : 'No'}
+                                        </Text>
+                                    </View>
+                                    <View style={styles.statusRow}>
+                                        <Text style={styles.textWrapper}>
+                                            Region:
+                                            {rally.region}
+                                        </Text>
+                                        <Text style={styles.textWrapper}>
+                                            EventRegion: {rally.eventRegion}
+                                        </Text>
+                                    </View>
+                                    <View style={styles.statusRow}>
+                                        <Text style={styles.textWrapper}>
+                                            UID: {rally.uid}
+                                        </Text>
+                                    </View>
+                                    <View style={styles.statusRow}>
+                                        <Text style={styles.textWrapper}>
+                                            Coordinator:{' '}
+                                            {rally?.coordinator?.name}
+                                        </Text>
+                                    </View>
+                                </View>
+                            </View>
+                        </Surface>
+                    </View>
+                </Collapsible>
+                {/*Code for Single Collapsible Ends*/}
             </View>
         </>
     );
@@ -93,10 +147,14 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginTop: 10,
     },
+    detailContainer: {
+        alignItems: 'center',
+    },
     surface: {
         // margin: 24,
         // height: 80,
-        width: '90%',
+        width: '100%',
+        borderRadius: 15,
         alignItems: 'center',
         justifyContent: 'center',
     },
@@ -104,6 +162,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: Colors.primary,
         padding: 10,
+        borderRadius: 15,
         width: '100%',
     },
     statusRow: {
@@ -126,5 +185,23 @@ const styles = StyleSheet.create({
     statusDataWrapper: {},
     textWrapper: {
         paddingHorizontal: 10,
+    },
+    content: {
+        padding: 20,
+        // backgroundColor: '#fff',
+    },
+    collapsibleHeaderRow: {
+        //backgroundColor: '#F5FCFF',
+        flexDirection: 'row',
+        padding: 10,
+        alignItems: 'center',
+    },
+    collapsibleHeaderText: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: '500',
+    },
+    collapsibleHeaderIcon: {
+        paddingLeft: 5,
     },
 });
