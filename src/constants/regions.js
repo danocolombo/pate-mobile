d571eeb28b5d666685927ffe04c5eec40c8d5bcf
Dano Colombo <danocolombo@gmail.com>
2022-07-27 05:56:57 -0400

    login determines region and eventRegion, can now remove from .env

---
+++ src/constants/regions.js
@@ -0,0 +1,12 @@
+export const REGION = {
+    GA: 'us#east#south#ga',
+    AL: 'us#east#south#al',
+    FL: 'us#east#south#fl',
+    SC: 'us#east#south#sc',
+    NC: 'us#east#south#nc',
+    TN: 'us#east#south#tn',
+    MS: 'us#east#south#ms',
+    KY: 'us#east#south#ky',
+    VA: 'us#east#south#va',
+    TT: 'us#test#south#tt',
+};
